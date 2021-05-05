2c269c1a2868bc10c1ac0a1bce2d6bd72fbb3648
mebangit123 <mebannongrum333@gmail.com>
2021-05-05 12:34:16 +0530

    Creating a simple LinkedList.

--- DataStructureDemo/src/linkedListDemo.java
+++ DataStructureDemo/src/linkedListDemo.java
@@ -1,7 +1,19 @@
-
 public class linkedListDemo {
 
 	public static void main(String[] args) {
 		System.out.println("LinkedList Demo");
+	}
+}
+class LinkedList {
+	Node head;
+	
+	class Node {
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			next =  null;
+		}
 	}
 }
