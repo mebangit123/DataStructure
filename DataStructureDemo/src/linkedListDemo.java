f09fb689e5594aeab1e73fc62c9eefddef55da65
mebangit123 <mebannongrum333@gmail.com>
2021-05-05 12:42:03 +0530

    Created likedList by adding three nodes

--- DataStructureDemo/src/linkedListDemo.java
+++ DataStructureDemo/src/linkedListDemo.java
@@ -2,18 +2,41 @@ public class linkedListDemo {
 
 	public static void main(String[] args) {
 		System.out.println("LinkedList Demo");
+		
+		LinkedList linkedList = new LinkedList();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+
+		linkedList.printList();
 	}
 }
+
 class LinkedList {
 	Node head;
-	
+
 	class Node {
 		int data;
 		Node next;
-		
-		Node(int data){
+
+		Node(int data) {
 			this.data = data;
-			next =  null;
+			next = null;
+		}
+	}
+
+	public void push(int data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+
+	public void printList() {
+		System.out.println("List : ");
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + "->");
+			temp = temp.next;
 		}
 	}
 }
