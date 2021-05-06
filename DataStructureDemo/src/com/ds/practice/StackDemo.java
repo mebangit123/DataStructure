44e01545b7f80b1d07cadf620a05e18913070c06
mebangit123 <mebannongrum333@gmail.com>
2021-05-06 15:02:47 +0530

    Added functionality to pop and peek element in a stack

--- DataStructureDemo/src/com/ds/practice/StackDemo.java
+++ DataStructureDemo/src/com/ds/practice/StackDemo.java
@@ -12,11 +12,26 @@ public class StackDemo {
 	public void printStack() {
 		link.printList();
 	}
+	public int popStack() {
+	int pop = link.pop();
+	return pop;
+	}
+	public int peek() {
+	if(link.head == null) {
+		System.out.println("Stack is empty");
+	}
+	return link.head.data;
+	}
 	public static void main(String[] args) {
 		StackDemo myStack = new StackDemo();
 		myStack.pushStack(70);
 		myStack.pushStack(30);
 		myStack.pushStack(56);
+		
+		int peek = myStack.peek();
+		System.out.println(peek);
+		int p = myStack.popStack();
+		System.out.println(p);
 		
 		myStack.printStack();
 	}
